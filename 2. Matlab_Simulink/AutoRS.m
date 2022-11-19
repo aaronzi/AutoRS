1e7987c4f457c88feb5bd287ee7b14c894b1d6be
Aaron Zielstorff <aaron.zi@web.de>
2022-11-19 17:12:41 +0100

    added lookup table

--- 2. Matlab_Simulink/AutoRS.m
+++ 2. Matlab_Simulink/AutoRS.m
@@ -5,4 +5,8 @@ clc;
 % KONSTANTEN AUFRUFEN
 global c;
 c = Konstanten();
-
+% 1. Setzen der Windturbinen Parameter für den Workspace
+load('CQ_CT_maps.mat')     % load aerodynamic maps CQ and CT (look up tables) of rotor blades
+mdlName = 'AutoRS_Simulation.slx';
+open(mdlName);
+sim(mdlName)
