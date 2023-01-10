7cfcf90b789fcb01c9cd4b658694c5848b5cd082
Aaron Zielstorff <aaron.zi@web.de>
2023-01-10 11:38:41 +0100

    Matlab Files zur Validierung des Antriebsstranges hinzugefügt

---
+++ 2. Matlab_Simulink/5. Validierung/Modell_Validierung.m
@@ -0,0 +1,7 @@
+%% Antriebsstrang-Modell
+
+% Winkelgeschwindigkeiten Phi_g_Punkt und Phi_r_Punkt
+Winkelgeschwindigkeiten();
+
+% Wellentorsion
+Wellentorsion();
--- 2. Matlab_Simulink/AutoRS.m
+++ 2. Matlab_Simulink/AutoRS.m
@@ -1,29 +1,30 @@
-% VOREINSTELLUNGEN
+%% VOREINSTELLUNGEN
 clear;
 clc;
 
-% DATEIPFADE HINZUFÜGEN
-addpath('./1. Konstanten/', './2. LookUp-Table/', './3. Simulationen/', './4. Linearisierung/');
+%% DATEIPFADE HINZUFÜGEN
+addpath('./1. Konstanten/', './2. LookUp-Table/', './3. Simulationen/', './4. Linearisierung/', './5. Validierung/'); % Hauptverzeichnisse
+addpath('./5. Validierung/1. Antriebsstrang/'); % Validierungsverzeichnisse
 
-% KONSTANTEN BERECHNEN
+%% KONSTANTEN BERECHNEN
 global param_const;
 param_const = Param_Const();
 
-% LOOK-UP-TABLES LADEN
+%% LOOK-UP-TABLES LADEN
 load('CQ_CT_maps.mat');
 
-% BERECHNUNG LAMBDA_OPT UND CP_MAX
+%% BERECHNUNG LAMBDA_OPT UND CP_MAX
 global param_opt;
 param_opt = Param_Opt(CQ_entries, lambda_array);
 
-% LINEARISIERUNGSKOEFFIZIENTEN
+%% LINEARISIERUNGSKOEFFIZIENTEN
 v_Test = 12;
 omega_r_Test = 12.1;
 theta_Test = 4;
 [k_v, k_omega_r, k_theta] = Linearisierungskoeffizienten(v_Test, omega_r_Test, theta_Test, param_const);
 
 
-% SIMULATION ÖFFNEN UND STARTEN
+%% SIMULATION ÖFFNEN UND STARTEN
 % mdlName = 'AutoRs_Simulation.slx';
 % open('./3. Simulationen/AutoRS_Simulation.slx');
 % sim(mdlName);
